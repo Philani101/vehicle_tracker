ea80a5def2fd4d7fbf72360680ae6ea013119581
Philani <philanimkalipi@gmail.com>
2025-12-19 11:47:18 +0200

    remove used imporrt

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import SvgIcon from '@mui/material/SvgIcon';
+
 
 interface EmptyStateProps {
   title: string;
